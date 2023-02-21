a0dd178c5a80bbeb128dc2a4e5f949b5d49f1d66
radzarom <aadamrobson@gmail.com>
2023-02-21 11:52:29 +0000

    enabled connection between clients via server, can see each others movement

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
-
-//Creates Pixi application
 let app = new PIXI.Application({width: 1400, height: 800})
-
-//Selects div to append app to in the DOM
-document.querySelector("#gameDiv").appendChild(app.view)
-
-
-//renders 60 star shapes in background
-for(let i = 0; i < 150; i++) {
-    const star = new PIXI.Graphics();
-    //draws a star at random width and height, with random number of points between 4-8, random radius 5-15
-    star.beginFill(0xADADAD).drawStar(Math.random() * app.screen.width, Math.random() * app.screen.height, Math.random() * 4 + 4, Math.random() * 5 + 1)
-    .endFill()
-
-    //add star to DOM
-    app.stage.addChild(star)
-}
 
 //create player sprite
 let player = PIXI.Sprite.from('ship.png')
 //scale dimensions
 player.width = 50
 player.height = 50
+
+//player1 start coords
+player.x = 0
+player.y = 0
+
 //x, y velocity
 player.dx = 0
 player.dy = 0
 //makes the position of sprite the center instead of upper left
 player.anchor.set(0.5)
 //sets player sprite to the center of screen
-player.position.set(app.screen.width/2, app.screen.height / 2)
+
 //add player sprite to DOM
-app.stage.addChild(player)
+
 //for tracking ship angle and mouse cursor coords, used for vectors
 let angle = 0
 let mousePosition;
 //keep track of all bullets on screeen
 let bullets = []
+let bulletsToSend = []
 let bulletSpeed = 7
 //keeps track of key presses
 let keys = {}
+
 
 //player y 
 let opponent = PIXI.Sprite.from('ship.png')
@@ -48,33 +38,139 @@ opponent.width = 50
 opponent.height = 50
 opponent.anchor.set(0.5)
 opponent.rotation = 0
-app.stage.addChild(opponent)
+let opponentBullets = []
+let bulletsReceived = []
 
-//allows mousechange to trigger changes from anywhere on page
-app.stage.hitArea = app.screen;
-app.stage.interactive = true;
+function createGame() {
 
-//on mouse move, determine angle needed for ship
-app.stage.on('mousemove', movePlayer);
+    const gameDiv = document.createElement('div')
+    gameDiv.setAttribute('id', 'gameDiv')
+    document.getElementById('body').appendChild(gameDiv)
+    //Creates Pixi application
+    
+    //Selects div to append app to in the DOM
+    document.querySelector("#gameDiv").appendChild(app.view)
 
-    function movePlayer(e) {
-        //updates global variable for this
-        mousePosition = e.data.global;
 
-        let playerPosition = player.getGlobalPosition();
+    //renders 60 star shapes in background
+    for(let i = 0; i < 150; i++) {
+    const star = new PIXI.Graphics();
+    //draws a star at random width and height, with random number of points between 4-8, random radius 5-15
+    star.beginFill(0xADADAD).drawStar(Math.random() * app.screen.width, Math.random() * app.screen.height, Math.random() * 4 + 4, Math.random() * 5 + 1)
+    .endFill()
 
-        //do math thing
-        angle = Math.atan2(mousePosition.y - playerPosition.y, mousePosition.x - playerPosition.x)
-
-        //calculate angle of rotation
-        player.rotation = angle + Math.PI/2
+    //add star to DOM
+    app.stage.addChild(star)
     }
 
-//on click fire bullet
-app.stage.on("pointerdown", fireBullet);
+    app.stage.addChild(player)
+    app.stage.addChild(opponent)
+
+    //allows mousechange to trigger changes from anywhere on page
+    app.stage.hitArea = app.screen;
+    app.stage.interactive = true;
+    //on mouse move, determine angle needed for ship
+    app.stage.on('mousemove', movePlayer);
+
+
+    //on click fire bullet
+    app.stage.on("pointerdown", fireBullet);
 
     //adds gameLoop function to the ticker so it is updated with each frame
     app.ticker.add(gameLoop);
+
+    //event listeners for key press
+    window.addEventListener("keydown", keysDown);
+    window.addEventListener("keyup", keysUp);
+}
+
+function movePlayer(e) {
+    //updates global variable for this
+    mousePosition = e.data.global;
+
+    let playerPosition = player.getGlobalPosition();
+
+    //do math thing
+    angle = Math.atan2(mousePosition.y - playerPosition.y, mousePosition.x - playerPosition.x)
+
+    //calculate angle of rotation
+    player.rotation = angle + Math.PI/2
+}
+
+
+// //Selects div to append app to in the DOM
+// document.querySelector("#gameDiv").appendChild(app.view)
+
+
+// //renders 60 star shapes in background
+// for(let i = 0; i < 150; i++) {
+//     const star = new PIXI.Graphics();
+//     //draws a star at random width and height, with random number of points between 4-8, random radius 5-15
+//     star.beginFill(0xADADAD).drawStar(Math.random() * app.screen.width, Math.random() * app.screen.height, Math.random() * 4 + 4, Math.random() * 5 + 1)
+//     .endFill()
+
+//     //add star to DOM
+//     app.stage.addChild(star)
+// }
+
+//create player sprite
+// let player = PIXI.Sprite.from('ship.png')
+// //scale dimensions
+// player.width = 50
+// player.height = 50
+// //x, y velocity
+// player.dx = 0
+// player.dy = 0
+// //makes the position of sprite the center instead of upper left
+// player.anchor.set(0.5)
+// //sets player sprite to the center of screen
+// player.position.set(app.screen.width/2, app.screen.height / 2)
+// //add player sprite to DOM
+// app.stage.addChild(player)
+// //for tracking ship angle and mouse cursor coords, used for vectors
+// let angle = 0
+// let mousePosition;
+// //keep track of all bullets on screeen
+// let bullets = []
+// let bulletSpeed = 7
+// //keeps track of key presses
+// let keys = {}
+
+// //player y 
+// let opponent = PIXI.Sprite.from('ship.png')
+// opponent.x = 100
+// opponent.y = 100
+// opponent.width = 50
+// opponent.height = 50
+// opponent.anchor.set(0.5)
+// opponent.rotation = 0
+// app.stage.addChild(opponent)
+
+// //allows mousechange to trigger changes from anywhere on page
+// app.stage.hitArea = app.screen;
+// app.stage.interactive = true;
+
+// //on mouse move, determine angle needed for ship
+// app.stage.on('mousemove', movePlayer);
+
+//     function movePlayer(e) {
+//         //updates global variable for this
+//         mousePosition = e.data.global;
+
+//         let playerPosition = player.getGlobalPosition();
+
+//         //do math thing
+//         angle = Math.atan2(mousePosition.y - playerPosition.y, mousePosition.x - playerPosition.x)
+
+//         //calculate angle of rotation
+//         player.rotation = angle + Math.PI/2
+//     }
+
+// //on click fire bullet
+// app.stage.on("pointerdown", fireBullet);
+
+//     //adds gameLoop function to the ticker so it is updated with each frame
+//     app.ticker.add(gameLoop);
 
     //gets vector for bullet using cursor coords and center position
     function fireBullet(e) {
@@ -89,12 +185,13 @@ app.stage.on("pointerdown", fireBullet);
         direction.y /= length;
 
         //create bullet, passing vector
-        let bullet = createBullet(direction);
+        let bullet = createBullet(player.x, player.y, direction);
         //add it to the array of existing bullets so it can be updated
         bullets.push(bullet);
+        bulletsToSend.push([player.x, player.y, direction])
     }
 
-    function createBullet(direction) {
+    function createBullet(x, y, direction) {
         //makes new bullet sprite
         let bullet = PIXI.Sprite.from('bullet.png');
 
@@ -106,8 +203,8 @@ app.stage.on("pointerdown", fireBullet);
         bullet.anchor.set(0.5);
 
         //offsets start of bullet path to front of ship rather than center
-        bullet.x = player.x + direction.x*40;
-        bullet.y = player.y + direction.y*40;
+        bullet.x = x + direction.x*40;
+        bullet.y = y + direction.y*40;
         bullet.speed = bulletSpeed;
 
         //determine appropriate rotation for bullet from ship angle
@@ -130,6 +227,17 @@ app.stage.on("pointerdown", fireBullet);
             }
         }
 
+
+        for(let i = 0; i < opponentBullets.length; i++) {
+            
+            opponentBullets[i].position.y += opponentBullets[i].direction.y*bulletSpeed
+            opponentBullets[i].position.x += opponentBullets[i].direction.x*bulletSpeed
+            //conditions for determining if bullet is offscreen go here, set to dead
+            if(opponentBullets[i].y < 0) {
+                opponentBullets[i].dead = true;
+            }
+        }
+
         //then remove bullets from DOM and bullets array
         for(let i = 0; i < bullets.length; i++) {
             if(bullets[i].dead) {
@@ -137,11 +245,17 @@ app.stage.on("pointerdown", fireBullet);
                 bullets.splice(i, 1);
             }
         }
+
+        //then remove bullets from DOM and bullets array
+        for(let i = 0; i < opponentBullets.length; i++) {
+            if(opponentBullets[i].dead) {
+                app.stage.removeChild(opponentBullets[i])
+                opponentBullets.splice(i, 1);
+            }
+        }
     }
 
-//event listeners for key press
-window.addEventListener("keydown", keysDown);
-window.addEventListener("keyup", keysUp);
+
 
     function keysDown(e) {
 
@@ -184,10 +298,7 @@ window.addEventListener("keyup", keysUp);
             else {
                 player.x += player.dx
             }
-    
 
-
-       
 
         if(player.x >= 0 && player.x <= app.view.width) {
 
@@ -235,23 +346,41 @@ window.addEventListener("keyup", keysUp);
 //anything inside of here gets ran on each tick, i think...
 function gameLoop(delta, direction) {
 
+    
     updateBullets(delta, direction);
     updatePosition();
+    bulletsReceived = [];
 
+    console.log(opponentBullets);
+
+    if(ws != 0) {
+
+        const messageBody = {
+            x: player.x,
+            y: player.y,
+            username,
+            angle,
+            bullets: bulletsToSend
+        };
+
+        bulletsToSend = [];
+        
+        ws.send(JSON.stringify(messageBody))
+    }
 }
 
-//Counts seconds elapsed since start
-let elapsed = 0.0;
+// //Counts seconds elapsed since start
+// let elapsed = 0.0;
 
-//applications ticker runs a callback every frame, delta is the amount of time since last tick
-app.ticker.add((delta) => {
-  // Add the time to our total elapsed time
-  elapsed += delta;
+// //applications ticker runs a callback every frame, delta is the amount of time since last tick
+// app.ticker.add((delta) => {
+//   // Add the time to our total elapsed time
+//   elapsed += delta;
 
-  //gives ship a hover effect
-  // Update sprite X position based on the cosine of our elapsed time.  
-  //elapsed/X changes speed of movement
-//   player.x = app.screen.width / 2 + Math.sin(elapsed/15.0) * 10.0;
-//   player.y = app.screen.height / 2 + Math.sin(elapsed/25.0) * 10.0;
-});
+//   //gives ship a hover effect
+//   // Update sprite X position based on the cosine of our elapsed time.  
+//   //elapsed/X changes speed of movement
+// //   player.x = app.screen.width / 2 + Math.sin(elapsed/15.0) * 10.0;
+// //   player.y = app.screen.height / 2 + Math.sin(elapsed/25.0) * 10.0;
+// });
 
--- startscreen.js
+++ startscreen.js
@@ -1,6 +1,158 @@
- function onStart (){
-    
-    document.getElementById("connection-feedback").innerText="waiting for the other player"
-    window.location = "index.html"
-    return false
+let teamName = "";
+let username = "";
+let otherPlayer = "";
+let startPos = [];
+let enemyStartPos = [];
+let startAngle;
+let enemyStartAngle;
+
+
+let currentPosX;
+let currentPosY;
+let currentAngle;
+let health = 100
+let currentBullets = [];
+
+
+let otherPosX;
+let otherPosY;
+let otherAngle;
+let otherHealth = 100
+let otherBullets = [];
+const speed = 10;
+
+//const site = "ws://localhost:8080";
+const site = "ws://spacewarserver.eu-4.evennode.com";
+//const site = "ws://spacewarsbackend.onrender.com";
+
+let ws = 0
+
+async function startGame() {
+   let usernameInput = document.getElementById("playername-input");
+   username = usernameInput.value;
+ 
+    ws = new WebSocket(site + "/unmatched?username=" + username);
+ 
+   ws.onopen = function (e) {
+     const messageBody = { username };
+     ws.send(JSON.stringify(messageBody));
+   };
+ 
+   function removeStarting() {
+   //   document.getElementById("startbutton")?.remove();
+   //   usernameInput.innerHTML = "";
+   //   usernameInput?.remove();
+   //   document.getElementById("spanname")?.remove();
+   document.getElementById("grid-container").remove();
+
+   }
+ 
+   ws.onmessage = (webSocketMessage) => {
+     const messageBody = JSON.parse(webSocketMessage.data);
+     
+     if (messageBody.message === "username") {
+       document.getElementById("connection-feedback").innerText =
+       "User name taken";
+     }
+     else if (messageBody.message === "waiting") {
+       document.getElementById("connection-feedback").innerText =
+       "Waiting for another player...";
+       //removeStarting();
+       return;
+     } else if (messageBody.message === "paired"){
+       removeStarting();
+       teamName = messageBody.teamName;
+       otherPlayer = messageBody.otherPlayer;
+       startPos = messageBody.startPos;
+       enemyStartPos = messageBody.enemyStartPos;
+       startAngle = messageBody.angle;
+       enemyStartAngle = messageBody.enemyAngle;
+ 
+      //  document.getElementById("players").innerText =
+      //    username + " vs " + otherPlayer;
+       //document.getElementById("connection-feedback").innerText = "";
+       playGame();
+     }
+   };
  }
+
+ async function playGame() {
+    ws = new WebSocket(site + "/matched?team=" + teamName);
+ 
+   window.onbeforeunload = function () {
+     const messageBody = {
+       lost: true,
+       username,
+     };
+     ws.send(JSON.stringify(messageBody));
+   };
+ 
+   ws.onopen = function (e) {
+
+    //Selects div to append app to in the DOM
+    createGame();
+    
+     player.x = startPos[0];
+     player.y = startPos[1];
+     opponent.x = enemyStartPos[0];
+     opponent.y = enemyStartPos[1];
+     player.rotation = startAngle;
+     opponent.rotation = enemyStartAngle;
+   };
+ 
+   ws.onmessage = (webSocketMessage) => {
+     const messageBody = JSON.parse(webSocketMessage.data);
+ 
+     if (messageBody.lost) {
+       if (username !== messageBody.username) {
+         alert(username + " won the game!");
+         return;
+       } else {
+         alert(username + " lost the game!");
+         return;
+       }
+     }
+ 
+     if (messageBody.username === username) {
+      //  currentPosX = messageBody.x;
+      //  currentPosY = messageBody.y;
+     } else {
+       opponent.x = messageBody.x;
+       opponent.y = messageBody.y;
+       opponent.rotation = messageBody.angle + Math.PI/2
+       opponentBullets = messageBody.bullets
+
+       for(let bullet in bulletsReceived) {
+
+        opponentBullets.push(createBullet(bullet[0], bullet[1], bullet[2]))
+    }
+     }
+   };
+ 
+  //  document.body.onkeydown = (evt) => {
+  //    if (evt.key === "ArrowUp") {
+  //      currentPosY -= speed;
+  //    } else if (evt.key === "ArrowDown") {
+  //      currentPosY += speed;
+  //    } else if (evt.key === "ArrowLeft") {
+  //      currentPosX -= speed;
+  //    } else if (evt.key === "ArrowRight") {
+  //      currentPosX += speed;
+  //    }
+  //    const messageBody = {
+  //      x: currentPosX,
+  //      y: currentPosY,
+  //      username,
+  //      currentAngle,
+  //      currentBullets,
+  //    };
+  //    ws.send(JSON.stringify(messageBody));
+  //  };
+ 
+ }
+//  function onStart (){
+    
+//     document.getElementById("connection-feedback").innerText="waiting for the other player"
+    
+//     return false
+//  }
