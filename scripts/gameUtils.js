075f354954b9de4841a72e139351c557553ad4d7
Mycroftd <mycroftd@hotmail.com>
2023-02-28 15:14:32 +0000

    fixing collection

--- scripts/gameUtils.js
+++ scripts/gameUtils.js
@@ -46,12 +46,13 @@ function powerUpCollision() {
     poweredUp = true;
     powerUpStatus = false;
     powerUpSend = true;
-    powerUp.x = Math.random() * app.view.width + 50
-    powerUp.y = Math.random() * app.view.height + 50
+    powerUp.x = Math.random() * app.view.width - 50
+    powerUp.y = Math.random() * app.view.height - 50
 
     powerUpCoords[0] = powerUp.x
     powerUpCoords[1] = powerUp.y
 
+    
     setTimeout(() => {
       powerUpSend = true
       powerUpStatus = true
